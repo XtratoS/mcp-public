9af0abac076891a02b1b536664a2d60808be511d
XtratoS <28851891+XtratoS@users.noreply.github.com>
2024-01-16 23:50:38 +0200

    initial

---
+++ docs/_app/immutable/chunks/coin.HdNPqGjT.js
@@ -0,0 +1,1 @@
+const n=""+new URL("../assets/coin.MXPGNT3H.png",import.meta.url).href;export{n as C};
---
+++ docs/_app/immutable/nodes/5.vQakco9L.js
@@ -0,0 +1,1 @@
+import{s as o,n as a}from"../chunks/scheduler._KR4ASMB.js";import{S as r,i,m as c,n as m,a as l,f as p}from"../chunks/index.PFYtDb1m.js";function f(n){let t;return{c(){t=c("EVENTS")},l(e){t=m(e,"EVENTS")},m(e,s){l(e,t,s)},p:a,i:a,o:a,d(e){e&&p(t)}}}class _ extends r{constructor(t){super(),i(this,t,null,f,o,{})}}export{_ as component};
